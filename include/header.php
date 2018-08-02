2ba60b3c51c3747885be8e7f0cd42a61e4c149f3
Adeline Patin <adeline.patin@live.fr>
2018-08-02 16:57:00 +0200

    ajout du formulaire

--- include/header.php
+++ include/header.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CRUD entreprise</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+
   </head>
 
   <body>
@@ -43,4 +45,3 @@
     </nav> <!-- Fin Nav Bootstrap -->
 
     <section class="container ma-section">
-      <h1 class="text-center mt-4 display-4">Bienvenue dans ma boutique</h1>
